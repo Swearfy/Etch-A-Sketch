e0bab1f4c82323298ed37e608224d560cfecea88
Swearfy <swearfy@gmail.com>
2023-02-06 21:53:52 +0000

    function for drawing area + mouseover listener

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const body = document.body;
 const drawBox = document.getElementById("drawBox");
+const reset = document.getElementById("reset");
 
-const loop = 256;
-
-for (let i = 0; i < loop; i++) {
-  const div = document.createElement("div");
-  div.style.width = "50px";
-  div.style.height = "50px";
-  div.style.backgroundColor = "white";
-  drawBox.append(div);
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "black";
-  });
+function box(size) {
+  let x = 800 / size;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const div = document.createElement("div");
+      div.style.width = x + "px";
+      div.style.height = x + "px";
+      div.style.backgroundColor = "white";
+      div.style.margin = "0";
+      div.style.padding = "0";
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+      });
+      drawBox.append(div);
+    }
+  }
 }
+
+box(5);
+
+reset.addEventListener("click", () => {
+  let x = drawBox.getElementsByTagName("div");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.backgroundColor = "white";
+  }
+});
